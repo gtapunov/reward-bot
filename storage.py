d635da7a2232a2205be70ee0f4f07de53fe021c9
gtapunov <gltapunov@gmail.com>
2025-04-06 14:32:34 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -3,6 +3,6 @@ import json
 def load_user_data():
     try:
         with open("user_data.json", "r", encoding="utf-8") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
+            user_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        user_data = {}
