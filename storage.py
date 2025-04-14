08a9154de8bfaab59c3f7329543b1c35b96ac054
gtapunov <gltapunov@gmail.com>
2025-04-14 16:23:49 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,15 +1,20 @@
 import json
+import os
 from json.decoder import JSONDecodeError
+
+# Путь к файлу в persistent диске
+DATA_PATH = "/data/user_data.json"
 
 def load_user_data():
     try:
-        with open("user_data.json", "r", encoding="utf-8") as f:
+        with open(DATA_PATH, "r", encoding="utf-8") as f:
             user_data = json.load(f)
-            print("Содержимое JSON:", f.read())  # временно, чтобы отладить
+            print("Содержимое JSON:", user_data)  # для отладки
     except (FileNotFoundError, JSONDecodeError):
         user_data = {}
     return user_data
 
 def save_user_data(data):
-    with open("user_data.json", "w", encoding="utf-8") as f:
+    os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
+    with open(DATA_PATH, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
